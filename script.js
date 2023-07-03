4b5db274ed6764b3974b8abb028007885980e13f
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-07-03 20:32:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+let table = document.getElementById("table");
+
 
 const getSum = () => {
 //Add your code here
-  
+  let arr = Array.from(document.querySelectorAll(".price"));
+	let sum = 0;
+	arr.forEach((item)=> {
+		 sum += parseInt(item.innerText);
+	});
+	let row =document.createElement("tr");
+	row.setAttribute('id',"ans");
+	let cell1 = document.createElement("td");
+	cell1.innerText = "Total is: ";
+	let cell2 = document.createElement("td");
+	cell2.innerText = sum;
+	row.appendChild(cell1)
+	row.appendChild(cell2);
+	table.appendChild(row);
 };
+
+getSumBtn.addEventListener("click", getSum);
 
 getSumBtn.addEventListener("click", getSum);
 
